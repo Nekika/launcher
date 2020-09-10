87d049f14d9c1bf0b9a8903ec2f4a14628718f04
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-09-10 19:33:33 +0200

    Components - Creating Detail The component where selected game's content will be displayed.

---
+++ src/components/Detail.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { isSidebarExpanded, isDownloading } from '../store';
+
+    export let selected;
+
+    $: gridColumnStart = $isSidebarExpanded ? 4 : 2;
+    $: gridRowEnd = $isDownloading ? -2 : -1;
+
+</script>
+
+<div id="detail" style="grid-area: 2 / {gridColumnStart} / {gridRowEnd} / -1" >
+    {#if selected}
+        <h2>{selected.title}</h2>
+    {:else}
+        <p class="hint">Select a game in the list beside</p>
+    {/if}
+</div>
+
+<style>
+    #detail {
+        grid-row: 2 / -2;
+        background-color: #C1A5A9;
+    }
+
+    h2, p {
+        text-align: center;
+    }
+</style>
