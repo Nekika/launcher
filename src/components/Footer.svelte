1dac175f925fe2abfaf0aad86b17c36bc851bde0
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-09-10 19:34:36 +0200

    Components - Creating Footer The component that will display a fake download progress bar. It's shown only when user clicks install button.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { isDownloading } from '../store';
+
+    $: display = $isDownloading ? 'inherit' : 'none';
+
+</script>
+
+<div style="display: {display}"></div>
+
+<style>
+    div {
+        grid-column: 1 / -1;
+        grid-row-start: -2;
+        background-color: #9A4C95;
+    }
+</style>
