4e255f7899faf1da0c81208cbadcf5b2c5b0e5ad
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-09-11 09:54:36 +0200

    Components - Adding install button to Detail This button is used to set the store isInstalling variable to true so the footer will be displayed

--- src/components/Detail.svelte
+++ src/components/Detail.svelte
@@ -1,16 +1,24 @@
 <script>
-    import { isSidebarExpanded, isDownloading } from '../store';
+    import { isSidebarExpanded, isInstalling } from '../store';
 
     export let selected;
 
     $: gridColumnStart = $isSidebarExpanded ? 4 : 2;
-    $: gridRowEnd = $isDownloading ? -2 : -1;
+    $: gridRowEnd = $isInstalling ? -2 : -1;
+
+    const runInstall = () => {
+        isInstalling.set(true);
+    }
 
 </script>
 
 <div id="detail" style="grid-area: 2 / {gridColumnStart} / {gridRowEnd} / -1" >
     {#if selected}
         <h2>{selected.title}</h2>
+        <input id="install-button"
+           type="button" 
+           value="Install"
+           on:click="{runInstall}">
     {:else}
         <p class="hint">Select a game in the list beside</p>
     {/if}
@@ -20,9 +28,20 @@
     #detail {
         grid-row: 2 / -2;
         background-color: #C1A5A9;
+        position: relative;
     }
 
     h2, p {
         text-align: center;
+    }
+
+    #install-button {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+
+    #install-button:hover {
+        cursor: pointer;
     }
 </style>
